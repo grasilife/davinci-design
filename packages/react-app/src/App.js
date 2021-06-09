166351a4e1450aad9da74ac57ed3d1aa212609f0
fuguoqiang1 <fuguoqiang1@100tal.com>
2021-06-09 19:31:51 +0800

    配置vscode配置

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Home from "./pages/Home/index";
+import Home from "@/pages/Home/index";
 import "./App.less";
 
 const App = () => (
