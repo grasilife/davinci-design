926be71c9e5309900392ec9e455563eeb9ee85b5
fuguoqiang1 <fuguoqiang1@100tal.com>
2021-06-09 17:51:45 +0800

    修复Expected server HTML to contain a matching <div> in <div>

--- packages/react-app/src/bootstrap.js
+++ packages/react-app/src/bootstrap.js
@@ -1,9 +1,12 @@
-import React from "react";
-import { hydrate } from "react-dom";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import { render, hydrate } from "react-dom";
 
-hydrate(<App />, document.getElementById("root"));
-
-if (module.hot) {
-  module.hot.accept();
-}
+const renderMethod = module.hot ? render : hydrate;
+renderMethod(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
--- packages/react-remote-base/src/bootstrap.js
+++ packages/react-remote-base/src/bootstrap.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import { hydrate } from 'react-dom';
-import App from './App';
+import React from "react";
+import { render, hydrate } from "react-dom";
+import App from "./App";
 
-hydrate(<App />, document.getElementById('root'));
-
-if (module.hot) {
-  module.hot.accept();
-}
+const renderMethod = module.hot ? render : hydrate;
+renderMethod(<App />, document.getElementById("root"));
